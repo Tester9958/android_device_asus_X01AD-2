110709c7761c3a20b3504dca519d790bc1bdc991
NP-XD <103098781+Tester9958@users.noreply.github.com>
2022-07-09 12:37:37 +0530

    X01AD: Remove More gms packages

--- remove_packages/Android.mk
+++ remove_packages/Android.mk
@@ -4,7 +4,7 @@ include $(CLEAR_VARS)
 LOCAL_MODULE := RemovePackages
 LOCAL_MODULE_CLASS := APPS
 LOCAL_MODULE_TAGS := optional
-LOCAL_OVERRIDES_PACKAGES :=  MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt GoogleFeedback RecorderPrebuilt Videos Ornament Tycho GoogleFeedback GoogleCamera MyVerizonServices SprintDM SprintHM OBDM_Permissions OemDmTrigger Snap Camera2 ScribePrebuilt Showcase SoundAmplifierPrebuilt SafetyHubPrebuilt PixelLiveWallpaperPrebuilt
+LOCAL_OVERRIDES_PACKAGES :=  MyVerizonServices SprintDM SprintHM YouTube YouTubeMusicPrebuilt GoogleFeedback RecorderPrebuilt Videos Ornament Tycho GoogleFeedback GoogleCamera MyVerizonServices SprintDM SprintHM OBDM_Permissions OemDmTrigger Snap Camera2 ScribePrebuilt Showcase SoundAmplifierPrebuilt SafetyHubPrebuilt DevicePolicyPrebuilt SCONE arcore ConnMetrics GoogleTTS OdadPrebuilt OemDmTrigger PixelBuds PixelLiveWallpaperPrebuilt
 LOCAL_UNINSTALLABLE_MODULE := true
 LOCAL_CERTIFICATE := PRESIGNED
 LOCAL_SRC_FILES := /dev/null
